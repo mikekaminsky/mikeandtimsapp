3246ad432881aab3fd7d07e739faf093bd155cc2
Tim Nichols <nichols@spotify.com>
2015-02-28 18:33:13 -0500

    Basic QR app sort of working, but not in simulator.

--- mikeandtimqr/mikeandtimqr/ViewController.m
+++ mikeandtimqr/mikeandtimqr/ViewController.m
@@ -10,6 +10,19 @@
 
 @interface ViewController ()
 
+@property (nonatomic) BOOL isReading;
+
+@property(nonatomic, strong) AVCaptureSession *captureSession;
+@property(nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+@property(nonatomic, strong) AVAudioPlayer *audioPlayer;
+
+
+-(BOOL)startReading;
+-(void)stopReading;
+-(void)loadBeepSound;
+
+
+
 @end
 
 @implementation ViewController
@@ -17,11 +30,112 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _isReading = NO;
+    
+    _captureSession = nil;
+    [self loadBeepSound];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - IBAction method implementation
+
+-(IBAction)startStopReading:(id)sender{
+    if(!_isReading){
+        if([self startReading]){
+            [_bbitemStart setTitle:@"Stop"];
+            [_lblStatus setText:@"Scanning for QR Code..."];
+        }
+    }
+    else{
+        [self stopReading];
+        [_bbitemStart setTitle:@"Start!"];
+    }
+    
+}
+
+//us
+-(BOOL)startReading{
+    NSError *error;
+    
+    AVCaptureDevice *captureDevice  = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    
+    if(!input){
+        NSLog(@"%@", [error localizedDescription]);
+        return NO;
+    }
+    
+    _captureSession = [[AVCaptureSession alloc] init];
+    [_captureSession addInput:input];
+    
+    AVCaptureMetadataOutput *captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    [_captureSession addOutput:captureMetadataOutput];
+    
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetadataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeQRCode]]; // we only want metadata
+    
+    
+    //show users what camera sees
+    _videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    [_videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [_videoPreviewLayer setFrame:_viewPreview.layer.bounds];
+    [_viewPreview.layer addSublayer:_videoPreviewLayer];
+    
+    [_captureSession startRunning];
+    
+    return YES;
+}
+
+
+-(void)stopReading{
+    [_captureSession stopRunning];
+    _captureSession = nil;
+    [_videoPreviewLayer removeFromSuperlayer];
+    
+}
+
+
+-(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
+    if (metadataObjects!= nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeQRCode]) { // do we have a QR code?
+            [_lblStatus performSelectorOnMainThread:@selector(setText:) withObject:[metadataObj stringValue] waitUntilDone:NO];
+            
+            [self performSelectorOnMainThread:@selector(stopReading) withObject:nil waitUntilDone:NO];
+            [_bbitemStart performSelectorOnMainThread:@selector(setTitle:) withObject:@"Start!" waitUntilDone:NO];
+            _isReading = NO;
+            
+            if (_audioPlayer) {
+                [_audioPlayer play];
+            }
+            
+        }
+    }
+}
+
+-(void)loadBeepSound
+{
+	NSString *beepFilePath = [[NSBundle mainBundle] pathForResource:@"beep" ofType:@"mp3"];
+	NSURL *beepURL = [NSURL URLWithString:beepFilePath];
+	NSError *error;
+
+	_audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:beepURL error:&error];
+	if (error) {
+		NSLog(@"Could not play beep file.");
+		NSLog(@"%@", [error localizedDescription]);
+	}
+	else{
+		[_audioPlayer prepareToPlay];
+	}
+}
+
 
 @end
