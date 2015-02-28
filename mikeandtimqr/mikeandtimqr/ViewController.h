e4b79deb7a8cf0c52773fae6239f0db58ee857a0
Tim Nichols <nichols@spotify.com>
2015-02-28 16:18:54 -0500

    Initial Commit

---
+++ mikeandtimqr/mikeandtimqr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mikeandtimqr
+//
+//  Created by nichols on 2/28/15.
+//  Copyright (c) 2015 nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
