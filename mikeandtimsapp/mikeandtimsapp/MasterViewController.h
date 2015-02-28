746389e61d0dc8b50799bee8b73d5e4958fcc142
Michael <ccUser@cc-notebook-19.local>
2015-02-28 15:56:21 -0500

    Starting xcode project

---
+++ mikeandtimsapp/mikeandtimsapp/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  mikeandtimsapp
+//
+//  Created by Michael on 2/28/15.
+//  Copyright (c) 2015 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
